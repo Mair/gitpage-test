265da418fafda0aa58613fc36f390b80dc45372b
mairs <mair.swartz@live.com>
2019-03-21 20:05:32 +1100

    Updates

---
+++ precache-manifest.210b1f79b20dd5e35394aea82fbb467a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Mair/gitpage-test/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f33aeab4ba36c409ac34",
+    "url": "/Mair/gitpage-test/static/js/runtime~main.baeff839.js"
+  },
+  {
+    "revision": "9f2778ce5e03f3463f1f",
+    "url": "/Mair/gitpage-test/static/js/main.8bdd7310.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/Mair/gitpage-test/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "9f2778ce5e03f3463f1f",
+    "url": "/Mair/gitpage-test/static/css/main.a69c05fe.chunk.css"
+  },
+  {
+    "revision": "5a851c64380fc7324d3f10c0b102ca4d",
+    "url": "/Mair/gitpage-test/index.html"
+  }
+];
